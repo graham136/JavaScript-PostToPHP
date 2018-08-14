0c1f475f50160f7c48d740f740dab0d2522e1116
Graham <42374809+graham136@users.noreply.github.com>
2018-08-14 15:22:50 +0200

    Add files via upload

---
+++ JavaScriptToDB.php
@@ -0,0 +1,22 @@
+	// Java Script post in html5 page via Rest
+<script>	
+				var postFormStr = "<form method='POST' action='" + '/URL/DBwriter.php' + "'>\n";
+				postFormStr += "<input type='hidden' name='" + 'Correct' + "' value='" + correctanswer + "'></input>";
+				postFormStr += "<input type='hidden' name='" + 'Answered' + "' value='" + answered + "'></input>";
+				postFormStr += "</form>";
+				var formElement = $(postFormStr);
+				$('body').append(formElement);
+				$(formElement).submit();
+</script>
+
+// dbwriter.php paste in dbwriter.php
+<?php
+
+	$correct = $_POST['Correct'];
+	$answered = $_POST['Answered'];
+		
+	$stmt = $conn->prepare("INSERT INTO yourtable ( correct,answered) VALUES (?, ?)");
+	if (!$stmt){die('Failed prepare'.$conn->error);}
+	$stmt->bind_param("ii", $correct,$answered);
+	$stmt->execute();
+?>
